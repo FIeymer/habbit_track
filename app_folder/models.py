7b64bd484ed1dc88dda95aba9be713f7b6a9b06c
Fleymer <fleymer@outlook.com>
2025-01-01 06:04:16 +0100

    fixing

--- app_folder/models.py
+++ app_folder/models.py
@@ -1,18 +1,15 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 
 from database import Base, engine
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
-    user_id = Column(primary_key=True, index=True)
+    user_id = Column(Integer, primary_key=True)
     username = Column(String)
     first_name = Column(String)
     last_name = Column(String)
     language = Column(String)
-
-
-
 
 Base.metadata.create_all(bind=engine)
